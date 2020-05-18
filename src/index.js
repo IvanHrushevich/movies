b9b00200030dbf23c90ffd498a2985205d3ecd61
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-05-18 22:08:56 +0300

    feat: implement basic webpack config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = <div>Hello from React!</div>;
+
+ReactDOM.render(app, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
