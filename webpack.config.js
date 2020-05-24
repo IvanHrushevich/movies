abf2e10694e9fc483a3dc12f084768054aa4aa7e
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-05-24 19:03:10 +0300

    feat: add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
@@ -64,6 +65,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'main.css',
